0547c9588c123390a29aa539950f7914a74ac15a
SovketKerimov <sovketkerimov9@gmail.com>
2026-04-26 21:33:13 +0400

    added new functions corrected mistakes

--- sign_log_function.py
+++ sign_log_function.py
@@ -31,6 +31,19 @@ def sign_up():
          print("Please try again")
          sign_up()
 def log_in():
-    pass
+    name = input("Enter your name:")
+    if name.strip() == "":
+        print("Please enter a valid name")
+        log_in()
+    password = input("Enter your password:")
+    for x in accounts:
+        if x.name == name and x.password == password:
+            name = x.name.capitalize()
+            Session.our_user = x
+            print(f"Welcome our dear user : {name}!")
+            return True
+    print("Please enter a correct username and password")
+    print("If you do not have an account create a new one")
+    return False
 def my_profile():
     pass
